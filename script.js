3b125a5caf66fbfed882d63ee481d8c23086484c
Avinash Manawadkar <140944342+AvinashManawadkar@users.noreply.github.com>
2026-02-24 13:35:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function loadMessage(){
+fetch("/message")
+.then(response => response.text())
+.then(data => alert(data));
+}
+
 function showMessage(){
-alert("Contact: avinashmanawadkar@gmail.com");
+
+fetch("/message")
+.then(response => response.text())
+.then(data => alert(data));
+
+}
+
+function loadMessage(){
+alert("Thank you for visiting my portfolio!");
 }
